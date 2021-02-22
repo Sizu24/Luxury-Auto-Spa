98321d54938504edcf5ac45dd7e723da6bb43d98
Shahetul Chisty <shahetulchisty@Shahetuls-MacBook-Pro.local>
2021-02-21 16:05:05 -0800

    Hamburger drop down in js, active class name for dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+const hamburger = document.querySelector(".js-burger");
+const navLinks = document.querySelector(".js-nav");
+const miniLogo = document.querySelector(".js-logo");
+
 const services = {
     service: "We detail any range of cars, trucks, SUVs, vans, luxury, and exotic vehicles.",
     exterior: "Exterior hand-wash, window cleaning, wheels and tires, wax, polish.",
@@ -14,6 +18,25 @@ serviceDescription.innerHTML = services.service;
 exteriorDescription.innerHTML = services.exterior;
 interiorDescription.innerHTML = services.interior;
 engineDescription.innerHTML = services.engine;
+
+
+/*
+    * Show nav links when clicking on hamburger
+    * Add ".active" class to nav links for styling
+    * Remove mini logo when displaying nav links
+*/
+hamburger.addEventListener("click", ()=>{
+    if(navLinks.style.display === "" || navLinks.style.display === "none"){
+        navLinks.classList.add("active");
+        navLinks.style.display = "block";
+        miniLogo.style.display = "none";
+    }else{
+        navLinks.classList.remove("active");
+        navLinks.style.display = "none";
+        miniLogo.style.display = "block";
+    }
+});
+
 
 
 // fade in main title an button. Set opacity of both to 0 in css
